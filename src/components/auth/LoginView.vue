5ab2245d0a3d31ac9db46ca5d4564656c2f20a12
Hoon-Hub <gwi01304@naver.com>
2022-12-08 18:29:05 +0900

    update

---
+++ src/components/auth/LoginView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <input type="email" />
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import "./assets/main.css";
+import { initializeApp } from "firebase/app";
 
-import './assets/main.css'
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
 
-const app = createApp(App)
+initializeApp(firebaseConfig);
 
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(router);
+
+app.mount("#app");
